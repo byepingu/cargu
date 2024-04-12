202e4684a279d6bc0d5cd0976e7a788fb3ee5b5b
carrot <yesolg90@gmail.com>
2024-04-12 15:30:23 +0900

    commit -m "ggg"

--- demo/src/main/frontend/src/App.js
+++ demo/src/main/frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './App.css';
+import './App.scss';
+import Home from './component/Home';
+
 
 function App() {
   const [data, setData] = useState();
@@ -8,14 +10,13 @@ function App() {
   useEffect(() => {
     axios.get('api/data')
       .then(res => setData(res.data))
-      .catch(res => console.log(err))
   }, [])
   return (
 
     <>
-      <div>
-        <p>"hellow,world",{data}</p>
-      </div>
+      <>
+      <Home/>
+      </>
     </>
   );
 }
